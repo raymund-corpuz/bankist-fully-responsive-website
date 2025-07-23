af2c001794e14c20fa3eac40d10cc1dbd88e88f1
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-24 07:57:30 +0800

    bankist-fully-responsive-website: Added a functionality on the tab component. Added a scroll behavior when the links are click

---
+++ data.js
@@ -0,0 +1,18 @@
+export const tabData = [
+  {
+    title: 'Send money in seconds — anytime, anywhere.',
+    content:
+      "Experience seamless and secure fund transfers with just a few taps. Whether you're paying a friend back or sending money across borders, Bankist ensures lightning-fast delivery with real-time tracking and zero delays.",
+    scale: 'active',
+  },
+  {
+    title: 'Top up your account instantly.',
+    content:
+      'Never run out of funds again. Instantly load money into your Bankist account from your linked bank, credit card, or e-wallet. Stay ready for every purchase, payment, or investment opportunity — all in real-time.',
+  },
+  {
+    title: 'Close your account without hassle.',
+    content:
+      'Need to move on? You’re in control. Bankist gives you the power to instantly and securely close your account, wipe personal data, and get final statements — no paperwork, no calls, no waiting.',
+  },
+];
--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-'use strict';
+// 'use strict';
+
+import { tabData } from './data.js';
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.btn--close-modal');
+// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+// const openModal = function () {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
+
+// tabbed components
+
+const instantTransfer = document.querySelector('.instant-transfer');
+const instantLoad = document.querySelector('.instant-load');
+const instantClosing = document.querySelector('.instant-closing');
+
+instantTransfer.addEventListener('click', () => getTabData(0));
+instantLoad.addEventListener('click', () => getTabData(1));
+instantClosing.addEventListener('click', () => getTabData(2));
+
+function getTabData(id) {
+  const tabMainTitle = document.querySelector('.content-main-title');
+  const tabMainContent = document.querySelector('.content-main-content');
+  tabMainTitle.textContent = tabData[id].title;
+  tabMainContent.textContent = tabData[id].content;
+}
+
+getTabData(0);
